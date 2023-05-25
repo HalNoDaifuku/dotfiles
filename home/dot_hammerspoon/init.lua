648513764da6ec9eec65ca8bdc7cedd64a691440
HalNoDaifuku <87297588+HalNoDaifuku@users.noreply.github.com>
2023-05-25 19:04:35 +0900

    🚀 feat(none): Change HotKey from Kitty to WezTerm

--- home/dot_hammerspoon/init.lua
+++ home/dot_hammerspoon/init.lua
@@ -1,14 +1,14 @@
 hs.hotkey.bind({"option"}, "space", function()
-    local app = hs.application.get("kitty")
+    local app = hs.application.get("WezTerm")
     if app then
         if not app:mainWindow() then
-            app:selectMenuItem({"kitty", "New OS window"})
+            app:selectMenuItem({"WezTerm", "New OS window"})
         elseif app:isFrontmost() then
             app:hide()
         else
             app:activate()
         end
     else
-        hs.application.launchOrFocus("kitty")
+        hs.application.launchOrFocus("WezTerm")
     end
 end)
