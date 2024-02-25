afbc3d41db286aa187a66508f6e34f76cdd6d85f
HalNoDaifuku <87297588+HalNoDaifuku@users.noreply.github.com>
2024-02-25 17:21:21 +0900

    🔨 fix(none): Fix backslash of WezTerm on MacOS

--- home/dot_wezterm.lua
+++ home/dot_wezterm.lua
@@ -1,20 +1,15 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
--- This table will hold the configuration.
-local config = {}
-
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
+-- This will hold the configuration.
+local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 config.color_scheme = 'Snazzy'
 config.font = wezterm.font 'FiraCode Nerd Font'
 config.window_background_image = '~/.config/neko52.png'
 config.window_background_opacity = 0.8
+config.send_composed_key_when_left_alt_is_pressed = true
 
 -- and finally, return the configuration to wezterm
 return config
