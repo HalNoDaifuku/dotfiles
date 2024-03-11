6a45e29b589821d7691c7ef27dcacdf38768110c
HalNoDaifuku <87297588+HalNoDaifuku@users.noreply.github.com>
2024-03-11 14:27:28 +0900

    🚀 feat(none): Chenge 'FiraCode Nerd Font' to 'Moralerspace Neon NF'

--- home/dot_wezterm.lua
+++ home/dot_wezterm.lua
@@ -6,7 +6,8 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 config.color_scheme = 'Snazzy'
-config.font = wezterm.font 'FiraCode Nerd Font'
+config.font = wezterm.font 'Moralerspace Neon NF'
+config.harfbuzz_features = { 'calt=1', 'dlig=1', 'liga=1' }
 config.window_background_image = '~/.config/neko52.png'
 config.window_background_opacity = 0.8
 config.send_composed_key_when_left_alt_is_pressed = true
